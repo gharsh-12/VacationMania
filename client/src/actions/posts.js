1b687f89e47aac12bdbd58461c65ec7ac461101c
Harsh <harshgopalani05@gmail.com>
2022-07-07 02:15:47 +0530

    final

---
+++ client/src/actions/posts.js
@@ -0,0 +1,29 @@
+import * as api from '../api';
+
+export const getPosts=()=>async(dispatch)=>{
+   try{
+     const {data}= await api.fetchPosts();
+     dispatch({type:'FETCH_ALL',payload:data})
+   }
+   catch(error){
+    console.log(error.message);
+   }
+}
+export const createPosts=(post)=>async(dispatch)=>{
+  try{
+    const {data}= await api.createPosts(post);
+    dispatch({type:'CREATE',payload:data})
+  }
+  catch(error){
+   console.log(error.message);
+  }
+}
+export const updatePost=(id,post)=>async(dispatch)=>{
+   try{
+     const {data} = await api.updatePost(id,post);
+     dispatch({type:'UPDATE',payload:data})
+   }
+   catch(error){
+     console.log(error.message);
+   }
+}
