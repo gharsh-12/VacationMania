e544dcde7070b909de33a6f7087cad716f6b3399
Harsh <harshgopalani05@gmail.com>
2022-07-06 23:47:01 +0530

    Initial commit

---
+++ server/models/postMessage.js
@@ -0,0 +1,23 @@
+// here we created the model how our databse will look like
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags:[String],
+  selectedFile:String,
+  likeCount:{
+    type:Number,
+    default: 0 
+  },
+  createdAt:{
+    type:Date,
+    default: new Date()
+  },
+})
+
+// converting Schema into model
+const PostMessage=mongoose.model('PostMessage',postSchema);
+
+export default PostMessage;
